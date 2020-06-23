b475d5d8a492d8314d2083fe9ef78bb66a228ead
金恒昱 <jinhengyu666@qq.com>
2020-06-23 17:11:56 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# zipack.github.io
+
+This repository stores zipack.github.io website using material-components-web as UI framework.
+
+## Test
+
+```shell
+node server.js
+```
---
+++ app/app.js
@@ -0,0 +1,26 @@
+import zipack from "./zipack.js";
+
+const input = document.querySelector("#input");
+const output = document.querySelector("#output");
+const jsonInfo = document.querySelector("#json-info");
+const zipackInfo = document.querySelector("#zipack-info");
+input.oninput = function () {
+  try {
+    const q = zipack.serialize(JSON.parse(input.value));
+    output.value = [...q]
+      .map((x) => x.toString(16).toUpperCase().padStart(2, 0))
+      .join(" ");
+    const jsonLength = new TextEncoder().encode(input.value).length;
+    jsonInfo.textContent = `${jsonLength} bytes`;
+    zipackInfo.textContent = `${q.length} bytes   (${Math.round(
+      (q.length * 100) / jsonLength
+    )}%)`;
+  } catch (err) {
+    output.value = "JSON Parse Error";
+    jsonInfo.textContent = ``;
+    zipackInfo.textContent = ``;
+  }
+};
+
+input.value = JSON.stringify({ name: "zipack", birth: 2020 });
+input.oninput();
